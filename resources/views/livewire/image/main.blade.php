05fc5e155edc0d962dd8fb76db263e86d45c5571
agustinus-robert <agustinus2h@gmail.com>
2021-08-18 10:13:12 +0700

    Munculkan gambar

--- resources/views/livewire/image/main.blade.php
+++ resources/views/livewire/image/main.blade.php
@@ -4,20 +4,36 @@
             @if (session()->has('message'))
                 <div class="col-md-12">
                     <div class="alert alert-warning">
-                        {{ session('pesan-ubah-gambar') }}
+                        {{ session('pesan-berhasil') }}
                     </div>
                 </div>
             @endif
         </div>
         
-        <div class="col-md-6 mx-auto mt-4">
-            <img style="margin:auto; width:500px; height:300px;" src="{{url('public/'.$image)}}" />
+        <div class="row">
+            <div class="col-md-6 mt-4">
+                <fieldset>
+                <legend>Gambar Original:</legend>
+                    <img style="margin:auto; width:500px; height:380px;" src="{{url('public/'.$image)}}" />
+                </fieldset> 
+            </div>
+            
+            <div class="col-md-6 mt-4">
+                <fieldset>
+                <legend>Gambar Edit:</legend>
+                   <?php 
+                   $image_path = 'public/temporary/temp_'.$image;
+                   if(file_exists($image_path)) { ?>
+                        <img style="margin:auto; width:500px; height:380px; border:1px solid black;" src="{{$image_path}}" />
+                   <?php } else { ?>
+                        <img style="margin:auto; width:500px; height:380px;" src="{{url('public/free_no.png')}}" />
+                   <?php } ?>
+                </fieldset> 
+            </div>
         </div>
         
-        <table>
-            <tr>
-                <td><button class="btn btn-primary" wire:click="bright($event.target.value)" value="{{$image}}">Bright</button></td>
-            </tr>
-        </table>
+        <hr>
+        <button class="btn btn-primary mb-4" wire:click="bright($event.target.value)" value="{{$image}}">Bright</button>
+        
     </div>
 </div>
