3cfde85e7dfb0da56d4d50a6fe9a43692049f660
agustinus-robert <agustinus2h@gmail.com>
2021-08-18 09:42:04 +0700

    Jika gambar ada pada awal method, maka hapus gambar, lalu buat gambar baru

--- app/Http/Livewire/Image/Main.php
+++ app/Http/Livewire/Image/Main.php
@@ -1,11 +1,34 @@
 <?php
 
 namespace App\Http\Livewire\Image;
-
+use Image;
+use File;
+use Session;
 use Livewire\Component;
 
 class Main extends Component
 {
+    public $image = 'gambar.jpg';
+    public $edited;
+    
+    private function hapus_temporary($val){
+        $image_path = 'temp_'.$val;  // Value is not URL but directory file path
+        if(File::exists($image_path)) {
+            File::delete(public_path('temporary/'. $image_path));
+            Session::flash('pesan-ubah-gambar',"Gambar berhasil diubah");
+        } else {
+            Session::flash('pesan-hapus-gambar-gagal',"Gambar gagal diubah");
+        }
+    }
+    
+    public function bright($value){
+        $this->hapus_temporary($value);
+        
+//        $img = Image::make('public/'.$value);
+//        $img->brightness(30);
+//        $img->save('public/temporary/'.'temp_'.$value);
+    }
+    
     public function render()
     {
         return view('livewire.image.main');
--- resources/views/livewire/image/main.blade.php
+++ resources/views/livewire/image/main.blade.php
@@ -1,3 +1,23 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <div class="container">
+        <div class="col-md-12">
+            @if (session()->has('message'))
+                <div class="col-md-12">
+                    <div class="alert alert-warning">
+                        {{ session('pesan-ubah-gambar') }}
+                    </div>
+                </div>
+            @endif
+        </div>
+        
+        <div class="col-md-6 mx-auto mt-4">
+            <img style="margin:auto; width:500px; height:300px;" src="{{url('public/'.$image)}}" />
+        </div>
+        
+        <table>
+            <tr>
+                <td><button class="btn btn-primary" wire:click="bright($event.target.value)" value="{{$image}}">Bright</button></td>
+            </tr>
+        </table>
+    </div>
 </div>
