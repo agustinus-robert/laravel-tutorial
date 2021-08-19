1acc0d4097b97b37404bcebd0e839395eba94aa3
agustinus-robert <agustinus2h@gmail.com>
2021-08-19 16:30:59 +0700

    crop image

--- app/Http/Livewire/Image/Main.php
+++ app/Http/Livewire/Image/Main.php
@@ -70,6 +70,17 @@ class Main extends Component
         return redirect(request()->header('Referer'));
     }
     
+    public function crop($value){
+        $this->hapus_temporary($value);
+        
+        $img = Image::make('public/'.$value);
+        $img->crop(400, 100, 60, 60);
+        $img->save('public/temporary/'.'temp_'.$value);
+        
+        Session::flash('pesan-berhasil', 'Gambar berhasil diubah');
+        return redirect(request()->header('Referer'));
+    }
+    
     public function render()
     {
         return view('livewire.image.main');
