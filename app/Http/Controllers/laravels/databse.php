47330f545ed788e743be69b4fa9b7cbda7104140
agustinus-robert <agustinus2h@gmail.com>
2021-08-20 11:53:26 +0700

    Buat fungsi model_when (Nama tutorial menyesuaikan nama method) dan model bernama GameModel

--- app/Http/Controllers/laravels/databse.php
+++ app/Http/Controllers/laravels/databse.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\laravels;
 
+use App\gameModel as gm;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class databse extends Controller
 {
     //
+    public function models_when(){
+        
+    }
 }
