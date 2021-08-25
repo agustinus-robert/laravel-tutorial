05a21e42645f89b0383c3096d745ad9069525cf5
agustinus-robert <agustinus2h@gmail.com>
2021-08-25 18:27:06 +0700

    select2 livewire kurang fleksible

--- app/Http/Livewire/Select/Select2C.php
+++ app/Http/Livewire/Select/Select2C.php
@@ -6,6 +6,18 @@ use Livewire\Component;
 
 class Select2C extends Component
 {
+    public $arr = [];
+    public $c;
+    public $data = [];
+    
+    public function mount(){
+        $this->data = [
+          'baju A', 'baju B', 'baju C'  
+        ];
+        
+    }
+    
+    
     public function render()
     {
         return view('livewire.select.select2-c');
--- resources/views/livewire/select/select2-c.blade.php
+++ resources/views/livewire/select/select2-c.blade.php
@@ -1,6 +1,12 @@
 <div>
-    <select data-pharaonic="select2" data-component-id="{{ $this->id }}" wire:model="country">
-        <option value="EG">Egypt</option>
-        <option value="TW">Taiwan</option>
-    </select>
+    <div class="container">
+        
+        <select data-pharaonic="select2" data-component-id="{{$this->id}}" wire:model="arr.a">
+            <?php foreach($data as $k => $v){ ?>
+                 <option value="{{$v}}">{{$v}}</option>
+            <?php } ?>
+        </select>
+
+      
+    </div>
 </div>
