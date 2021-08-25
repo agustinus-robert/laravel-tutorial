3402c79061e693dc521267a76703ceddbd589110
agustinus-robert <agustinus2h@gmail.com>
2021-08-25 13:20:22 +0700

    Datatbles berhasil diterapkan

--- app/Http/Livewire/GameDatatables.php
+++ app/Http/Livewire/GameDatatables.php
@@ -3,19 +3,19 @@
 namespace App\Http\Livewire;
 
 use App\gameModel as gm;
+use Mediconesystems\LivewireDatatables\Column;
+use Mediconesystems\LivewireDatatables\NumberColumn;
 use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
 
 class GameDatatables extends LivewireDatatable
 {
     public function builder()
     {
-        //
         return gm::query();
     }
 
     public function columns()
     {
-        //
         return [
           NumberColumn::name('id')
                 ->label("ID"),
